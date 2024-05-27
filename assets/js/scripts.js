b7d780f327154964cc80365e072736c862e6c7f0
JustShoveIt <himadri.dares@gmail.com>
2024-05-28 01:49:59 +0545

    add files

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+document
+	.querySelector('.newsletter button')
+	.addEventListener('click', function (event) {
+		event.preventDefault();
+		alert('Thank you for subscribing to our newsletter!');
+	});
