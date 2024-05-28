ae57148f355bb0f121370109d83726bb92e26288
JustShoveIt <himadri.dares@gmail.com>
2024-05-29 00:34:25 +0545

    finalise footer design for now finalise design for the time being only work on refactoring and improving performance from now! improved code practices

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,10 +21,3 @@ window.onload = function () {
 		modal.style.display = 'none';
 	};
 };
-
-document
-	.querySelector('.newsletter>button')
-	.addEventListener('click', function (event) {
-		event.preventDefault();
-		alert('Thank you for subscribing to our newsletter!');
-	});
